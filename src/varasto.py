6386ef3ece354243fbe5a276c6dc66c2c70145c8
Jenni Nylund <jenni.esprit@gmail.com>
2021-11-11 13:32:57 +0200

    lisätty pylint

--- src/varasto.py
+++ src/varasto.py
@@ -1,11 +1,15 @@
+def tarkista_tilavuus(luku):
+    if luku > 0.0:
+        return luku
+    #virheellinen, nollataan
+    luku = 0.0
+    return luku
+
+
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        if tilavuus > 0.0:
-            self.tilavuus = tilavuus
-        else:
-            # virheellinen, nollataan
-            self.tilavuus = 0.0
-        
+        self.tilavuus = tarkista_tilavuus(tilavuus)
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
@@ -31,7 +35,7 @@ class Varasto:
     def ota_varastosta(self, maara):
         if maara < 0:
             return 0.0
-        if maara > self.saldo: 
+        if maara > self.saldo:
             kaikki_mita_voidaan = self.saldo
             self.saldo = 0.0
 
@@ -40,6 +44,6 @@ class Varasto:
         self.saldo = self.saldo - maara
 
         return maara
-    
+
     def __str__(self):
         return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
